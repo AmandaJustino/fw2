785a919a235822ceec882e7d053d13c1826c851b
AmandaJustino <amanda.inocencio@estudante.ifms.edu.br>
2024-10-28 15:23:18 +0000

    aula28/10

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,19 +1,40 @@
 import { Container } from "react-bootstrap";
 import Menu from "./component/navbar";
 import Footer from "./component/footer";
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 export default function Promessa() {
+    var compara = "";
+
+    if("0" === 0) {
+        compara = "verdadeiro";
+    } else {
+        compara = "falso";
+    }
+
+
+
     const [prometido, setPrometido] = useState(" ")
+    useEffect(()  => {});
     setTimeout(() => {
-        setPrometido("é uma dívida")
+        setPrometido(mudaTexto("Olá texto novo"));
         document.title="Promessa é divida"
-    }, 2000);
-    return<>
+    }, 4000);
+
+}); 
+    return <>
      <Menu />
      <Container>
-        Promessa {prometido}
+     Promessa {prometido === "" ? <img src="./gif fw2.gif" width={50} height={40}/> : prometido}
      </Container>
      <Footer />
         </>
+}
+
+function mudaTexto(texto) {
+    return new Promisse ((resolva) => 
+     setTimeout(() => {
+        resolva(texto);
+     }, 1000)
+    );
 }
